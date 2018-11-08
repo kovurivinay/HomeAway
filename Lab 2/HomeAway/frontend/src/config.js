5a54fa435dd34ada10cef175eb5938505b48672d
Vinay Kovuri <kovurivinay@gmail.com>
2018-11-08 01:19:40 -0800

    changes

--- Lab 2/HomeAway/frontend/src/config.js
+++ Lab 2/HomeAway/frontend/src/config.js
@@ -1,6 +1,6 @@
-export const React_IP="http://localhost:"
-//export const React_IP="http://52.8.226.152:"
+//export const React_IP="http://localhost:"
+export const React_IP="http://52.8.226.152:"
 export const React_Port=3000;
-export const Node_IP="http://localhost:"
-//export const Node_IP="54.241.228.163:"
+//export const Node_IP="http://localhost:"
+export const Node_IP="54.241.228.163:"
 export const Node_Port=3001;
